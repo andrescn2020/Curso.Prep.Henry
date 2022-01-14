8b5940813e9177e6a88847cb328851a4eafae1fc
andrescn2020 <andrescn_2010@hotmail.com>
2022-01-13 23:37:44 -0300

    Homework 4 Done

--- 05-JS-IV/homework/README.md
+++ 05-JS-IV/homework/README.md
@@ -10,6 +10,18 @@
 	* Bucle `for…in`
 	* Notación de puntos vs notación de corchetes
 
+	* Los objetos son cosas con propiedades que definen sus caracteristicas.
+
+	* Las propiedades de un objeto tienen un nombre y valor, puede tener cualquier tipo de dato excepto undefined.
+
+	* Se llama metodos a las funciones establecidas en la propiedad de un objeto.
+
+	* El bucle for in sirve para recorrer todas las claves de un objeto.
+
+	* La notación de puntos solo funciona con nombres de propiedades que son válidos y la notación de corchetes espera una expresión que se evalúa como una cadena.
+
+
+
 2. Desde la carpeta `Prep` en la carpeta donde clonaste el repo, ingresa el comando `npm test JSIV.test.js` para correr los tests automatizados. Al principio, todos los tests estarán fallados/rotos. Encontrarás las funciones para hacer pasar los tests en el archivo `homework.js`.
 
 ### Aca tendras acceso a las [Soluciones](https://github.com/atralice/Curso.Prep.Henry/blob/solution/05-JS-IV/homework/homework.js)
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -102,12 +102,19 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if (password === usuario.password){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
-  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
+  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -115,6 +122,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -123,6 +132,12 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+
+  for (let i = 0; i < usuarios.length; i++) {
+    usuarios[i].esPremium = true;
+    
+  }
+ return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -132,6 +147,14 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let suma = 0;
+
+for (let i = 0; i < usuario.posts.length; i++) {
+  suma = suma + usuario.posts[i].likes;
+  
+}
+
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -144,7 +167,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  
+  producto.calcularPrecioDescuento = function () {
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
